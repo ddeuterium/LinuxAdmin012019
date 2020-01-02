e66a3dabdaecaa3a87e1963935524c483e09235c
Pavel Andreev <shaadowsky@gmail.com>
2020-01-02 13:38:39 +0800

    finished

--- hw25. Dynamic web/wsgi.py
+++ hw25. Dynamic web/wsgi.py
@@ -1,4 +1,4 @@
 def application(environ, start_response):
         start_response('200 OK', [('Content-Type', 'text/html')])
-        return ["<h1 style='color:blue'>Testing Success!</h1>"]
+        return ["<h1 style='color:blue'>Flask (Python) server is running!</h1>"]
  
